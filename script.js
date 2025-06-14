3fd762828802c806cb908e4befaa60badd180012
Sobinjm <jmsobin@gmail.com>
2025-06-14 13:02:08 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function calculate(operandOne, operandTwo){
+var result = operandOne + operandTwo;
+console.log(result);
+
+// var demo;
+
+console.log(demo);
+
+demo = 'dummy data';
+
+
+var demo2 = '';
+demo2 = null;
+
+console.log(demo2);
+
+
+
+}
